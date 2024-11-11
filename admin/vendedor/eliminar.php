b18d1d3c5745055f2e2c2c6d38ce0dab3223cdb5
AlejandLeo <167577922+AlejandLeo@users.noreply.github.com>
2024-11-11 10:11:04 -0400

    Update eliminar.php

---
+++ admin/vendedor/eliminar.php
@@ -0,0 +1,35 @@
+<?php
+$inicio = false;
+include "../../includes/templates/header.php";
+
+$idv = $_GET['cod'] ?? null;
+
+if ($idv) {
+    echo "El id del vendedor es: " . htmlspecialchars($idv);
+
+    include "../../includes/config/database.php";
+    $db = conectarBD();
+    $consql = "UPDATE vendedores SET estado = 'inactivo' WHERE idvendedor = $idv";
+    $res = mysqli_query($db, $consql);
+
+    
+    if ($res) {
+        echo "
+        <script>
+            alert('El vendedor fue eliminado correctamente');
+            location.href = 'listarvendedores.php';
+        </script>";
+    } else {
+        echo "Error al modificar el vendedor";
+        
+    }
+} else {
+    echo "<p>ID de vendedor no válido.</p>";
+}
+?>
+<main class="contenedor seccion">
+    <h1>Eliminar Vendedor</h1>
+</main>
+<?php
+include "../../includes/templates/footer.php";
+?>
